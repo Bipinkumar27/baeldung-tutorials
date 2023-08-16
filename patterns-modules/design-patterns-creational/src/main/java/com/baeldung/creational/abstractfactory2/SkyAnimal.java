157d762acfd66501867851fcbce7480592468d55
Bipinkumar27 <sayy2bipin@gmail.com>
2023-08-16 21:51:55 +0530

    JAVA-1597: Initial commit made for abstractfactory example 2

--- patterns-modules/design-patterns-creational/src/main/java/com/baeldung/creational/abstractfactory2/SkyAnimal.java
+++ patterns-modules/design-patterns-creational/src/main/java/com/baeldung/creational/abstractfactory2/SkyAnimal.java
@@ -1,11 +1,6 @@
 package com.baeldung.creational.abstractfactory2;
 
 public class SkyAnimal extends Animal {
-    /*@Override
-    void create() {
-        System.out.println(" created a sky animal");
-    }*/
-
     SkyAnimal(EnumAnimalType type, EnumEra era, String description) {
         super(type, era);
         super.setDescription(description);
